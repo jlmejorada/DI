64915b013539eca7fd6c607a46c2243b0c408ef2
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-10-18 13:54:25 +0200

    Ejercicios

---
+++ Tema08/Ejercicio01/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejercicio01
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
