6b8e114b440e7f1f0a141b9f5a6cbfb29cfea9d3
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-10-21 14:39:45 +0200

    cosa

---
+++ Tema08/Ejercicio05/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Ejercicio05Ent;
+using System.Collections.ObjectModel;
+
+namespace Ejercicio05
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        ObservableCollection <clsPersona> personas = new ObservableCollection<clsPersona>();
+        public ObservableCollection<clsPersona> Persona { get { return personas; } }
+        public MainPage()
+        {
+            InitializeComponent();
+            personas.Add(new clsPersona() { Nombre = "Jose Luis", Apellido = "Mejorada Marín" });
+            personas.Add(new clsPersona() { Nombre = "Marta", Apellido = "Rquejo Merino",});
+            personas.Add(new clsPersona() { Nombre = "Ruben", Apellido = "García Barquero" });
+            PersonaListView.ItemsSource = personas;
+        }
+
+
+    }
+
+}
---
+++ Tema08/Ejercicio05Clases/clsPersona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio05Ent
+{
+    public class clsPersona
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+    }
+}
