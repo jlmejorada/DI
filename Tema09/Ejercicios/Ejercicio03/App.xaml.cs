5eb31b590eeab085268ba3427c58eaba92a258b6
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-11-11 18:54:11 +0100

    Intento de los ejercicios

---
+++ Tema09/Ejercicios/Ejercicio03/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Ejercicio03.Views;
+
+namespace Ejercicio03
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
